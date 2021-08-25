1ab594c934494b5b2d6065129035685a6cead0b2
Sofia <sofiaaldanabenitez@gmail.com>
2021-08-25 11:54:15 -0300

    clase 2

---
+++ ejercicioI04calculadora/Biblioteca/Calculadora.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    public class Calculadora
+    {
+        public static float Calcular(float primerOperando, float segundoOperando, char operador)
+        {
+            float resultado;
+
+            switch(operador)
+            {
+                case '+':
+                    resultado = primerOperando + segundoOperando;
+                    break;
+                case '-':
+                    resultado = primerOperando - segundoOperando;
+                    break;
+                case '*':
+                    resultado = primerOperando * segundoOperando;
+                    break;
+                case '/':
+                    if(Validar(segundoOperando))
+                    {
+                        resultado = primerOperando / segundoOperando;
+                    }
+                    else
+                    {
+                        resultado = float.NaN;
+                    }
+                   
+                    break;
+                default:
+                    resultado = float.NaN;
+                    break;
+            }
+           
+
+            return resultado;
+        }
+            
+        private static bool Validar(float segundoOperando)
+        {
+            bool resultado = true;
+
+            if(segundoOperando == 0)
+            {
+                resultado = false;
+            }
+
+            return resultado;
+        }
+    }
+}
---
+++ ejercicioI04calculadora/ejercicioI04calculadora/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Biblioteca;
+
+/*
+ * Se le debe pedir al usuario que ingrese dos números 
+ * y la operación que desea realizar (ingresando el caracter +, -, * o /).
+ */
+namespace ejercicioI04calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float primerNumeroIngresado;
+            float segundoNumeroIngresado;
+            char operador;
+            float resultado;
+
+
+            
+            Console.WriteLine("Ingrese el primer operando: ");
+            if(!(float.TryParse(Console.ReadLine(), out primerNumeroIngresado)))
+            {
+                Console.WriteLine("Error.Ingrese el primer operando: ");
+                float.TryParse(Console.ReadLine(), out primerNumeroIngresado);
+            }
+
+            Console.WriteLine("Ingrese el segundo operando: ");
+            if(!(float.TryParse(Console.ReadLine(), out segundoNumeroIngresado)))
+            {
+                Console.WriteLine("Error.Ingrese el segundo operando: ");
+                float.TryParse(Console.ReadLine(), out segundoNumeroIngresado);
+            }
+
+            Console.WriteLine("Ingrese la operacion que desea realizar ( +, -, * o /): ");
+            operador = char.Parse(Console.ReadLine());
+
+            resultado = Calculadora.Calcular(primerNumeroIngresado, segundoNumeroIngresado, operador);
+
+            if(resultado!=float.NaN)
+            {
+                Console.WriteLine($"Resultado: {resultado}");
+            }
+            else
+            {
+                Console.WriteLine("No se pudo realizar la operacion");
+            }
+            
+        }
+    }
+}
