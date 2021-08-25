8f93d35095689cd90451cdc3d0b44e362a4e4d18
Sofia <sofiaaldanabenitez@gmail.com>
2021-08-25 14:37:03 -0300

    clase 2

---
+++ ejercicioI06areas/Biblioteca/CalculadoraDeArea.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    public class CalculadoraDeArea
+    {
+        public static double CalcularAreaCuadrado(double longitudLado)
+        {
+            double area = Math.Pow(longitudLado, 2);
+
+            return area;
+        }
+
+        public static double CalcularAreaTriangulo(double bas, double altura) 
+        {
+            double area = (bas * altura) / 2;
+            return area;
+        
+        }
+
+        public static double CalcularAreaCirculo(double radio)
+        {
+            double area = Math.PI * Math.Pow(radio,2);
+
+            return area;
+        }
+    }
+}
---
+++ ejercicioI06areas/ejercicioI06areas/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Biblioteca;
+
+namespace ejercicioI06areas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("A. Calcular el Area de un cuadrado");
+            Console.WriteLine("B. Calcular el Area de un triangulo");
+            Console.WriteLine("C. Calcular el Area de un circulo");
+            Console.WriteLine("Que operacion quiere realizar?: ");
+
+            char opcion = char.Parse(Console.ReadLine());
+
+            double resultado = double.NaN;
+
+            switch (opcion)
+            {
+                case 'a':
+                    Console.WriteLine("Ingrese la longitud del lado: ");
+                    int longitud = int.Parse(Console.ReadLine());
+
+                    resultado = CalculadoraDeArea.CalcularAreaCuadrado(longitud);
+
+                    break;
+                case 'b':
+                    Console.WriteLine("Ingrese la base del triangulo: ");
+                    int bas = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Ingrese la altura del triangulo: ");
+                    int altura = int.Parse(Console.ReadLine());
+
+                    resultado = CalculadoraDeArea.CalcularAreaTriangulo(bas, altura);
+
+                    break;
+                case 'c':
+                    Console.WriteLine("Ingrese el radio del circulo: ");
+                    int radio = int.Parse(Console.ReadLine());
+
+                    resultado = CalculadoraDeArea.CalcularAreaCirculo(radio);
+
+                    break;
+                default:
+                    Console.WriteLine("Opcion invalida");
+                    break;
+
+            }
+
+            if(resultado!=double.NaN)
+            {
+                Console.WriteLine($"Resultado: {resultado}");
+            }
+            
+
+        }
+    }
+}
