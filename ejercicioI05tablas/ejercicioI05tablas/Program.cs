d30c95cf2864c07948bea95699957ab6fcb42c65
Sofia <sofiaaldanabenitez@gmail.com>
2021-08-29 15:23:17 -0300

    clase 2

---
+++ ejercicioI05tablas/ejercicioI05tablas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace ejercicioI05tablas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroIngresado;
+            string tabla;
+
+            Console.WriteLine("Ingrese un numero: ");
+            numeroIngresado = int.Parse(Console.ReadLine());
+            tabla=Tablas.DevolverTabla(numeroIngresado);
+
+            Console.WriteLine(tabla);
+
+        }
+    }
+}
---
+++ ejercicioI05tablas/ejercicioI05tablas/Tablas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioI05tablas
+{
+    class Tablas
+    {
+        public static string DevolverTabla(int numero)
+        {
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine($"Tabla de multiplicar del numero {numero}");
+
+            for (int i=0; i<10; i++)
+            {   
+                stringBuilder.AppendLine($"{numero} x {i} = {numero * i}");
+            }
+
+            string tabla = stringBuilder.ToString();
+
+            return tabla;
+        }
+    }
+}
