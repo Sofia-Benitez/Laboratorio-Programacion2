8cfa0c1d4e0aa85f85e70a6f6da1c4348a0dd673
Sofia <sofiaaldanabenitez@gmail.com>
2021-08-29 17:24:56 -0300

    clase 2

---
+++ ejercicioI09triangulo/ejercicioI09triangulo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ejercicioI09triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int altura;
+
+            Console.WriteLine("Ingrese la altura deseada para el triangulo: ");
+
+            if(int.TryParse(Console.ReadLine(), out altura))
+            {
+                for(int i=1; i<=altura; i++)
+                {
+                    for(int j=1; j<=altura-i; j++)
+                    {
+                        Console.Write("");
+                    }
+                    for (int j = 1; j <= 2*i-1; j++)
+                    {
+                        Console.Write("*");
+                    }
+
+
+                    Console.Write("\n");
+                }
+            }
+
+
+        }
+    }
+}
