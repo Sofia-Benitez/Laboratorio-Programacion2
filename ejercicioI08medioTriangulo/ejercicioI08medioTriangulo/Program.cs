660f3eb1d9eb7622c70209431021e2d54bbf95dc
Sofia <sofiaaldanabenitez@gmail.com>
2021-08-29 17:02:52 -0300

    clase 2

---
+++ ejercicioI08medioTriangulo/ejercicioI08medioTriangulo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ejercicioI08medioTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la altura deseada para el triangulo");
+            int altura = int.Parse(Console.ReadLine());
+
+            for(int i=1; i<=altura;i++)
+            { 
+                for (int j=1; j<=i; j++)
+                {
+                    if(j==1)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write("**");
+                    }
+                    
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
