f5e42ec9a518403f64ae3131185f5c22e67cb15a
Sofia <sofiaaldanabenitez@gmail.com>
2021-08-29 17:25:49 -0300

    clase 2

---
+++ ejercicio I07pitagoras/ejercicio I07pitagoras/Hipotenusa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_I07pitagoras
+{
+    class Hipotenusa
+    {
+
+        public static float AplicarPitagoras(float bas, float altura)
+        {
+            float hipotenusa;
+            hipotenusa = (float) Math.Sqrt(Math.Pow(bas,2) + Math.Pow(altura,2));
+
+            return hipotenusa;
+        }
+    }
+}
---
+++ ejercicio I07pitagoras/ejercicio I07pitagoras/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ejercicio_I07pitagoras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float baseIngresada;
+            float alturaIngresada;
+            float hipotenusa=0;
+            bool baseValida = false;
+            bool alturaValida = false;
+
+            Console.WriteLine("Pitágoras estaría orgulloso");
+            Console.WriteLine("Ingresa la base del triangulo (en cm): ");
+            baseValida = float.TryParse(Console.ReadLine(), out baseIngresada);
+
+            Console.WriteLine("Ingresa la altura del triangulo (en cm): ");
+            alturaValida = float.TryParse(Console.ReadLine(), out alturaIngresada);
+
+            if(alturaValida && baseValida)
+            {
+                hipotenusa = Hipotenusa.AplicarPitagoras(baseIngresada, alturaIngresada);
+            }
+            else
+            {
+                Console.WriteLine("Error. Datos invalidos");
+            }
+            
+
+            Console.WriteLine($"La hipotenusa del triangulo es {hipotenusa}");
+
+        }
+    }
+}
