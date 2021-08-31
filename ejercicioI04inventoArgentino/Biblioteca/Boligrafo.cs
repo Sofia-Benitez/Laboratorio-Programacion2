79664c84f8d1921b490fb8ab2bafdbf27bae99b5
Sofia <sofiaaldanabenitez@gmail.com>
2021-08-31 14:54:54 -0300

    clase 3

---
+++ ejercicioI04inventoArgentino/Biblioteca/Boligrafo.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text;
+
+namespace Biblioteca
+{
+    public class Boligrafo
+    {
+        private ConsoleColor color;
+        private short tinta;
+        private static short cantidadTintaMaxima;
+
+        static Boligrafo()
+        {
+            cantidadTintaMaxima = 100;
+        }
+
+        public Boligrafo(ConsoleColor color, short tinta)
+        {
+            this.color = color;
+            this.tinta = tinta;
+        }
+
+        public ConsoleColor GetColor()
+        {
+            return color;
+        }
+
+        public short GetTinta()
+        {
+            return tinta;
+        }
+
+        private void SetTinta(short tinta)
+        {
+            
+           
+            if(tinta!=cantidadTintaMaxima)
+            {
+                short tintaActual;
+                tintaActual = this.tinta += tinta;
+                if (tintaActual >= 0 && tintaActual <= cantidadTintaMaxima)
+                {
+                    this.tinta = tintaActual;
+                }
+            }
+            else
+            {
+                this.tinta = tinta;
+            }
+          
+        }
+
+        public void Recargar()
+        {
+            SetTinta(cantidadTintaMaxima);
+        }
+
+        public string Pintar(short gasto)
+        {
+            string dibujo;
+            StringBuilder sb = new StringBuilder();
+
+            if(this.tinta>= 0)
+            {
+                for (int i = 1; i <= -gasto; i++)
+                { 
+                    sb.Append("*");
+                    
+                }
+                SetTinta(gasto);
+            }
+               
+            dibujo = sb.ToString();
+
+            return dibujo;
+        }
+    }
+}
---
+++ ejercicioI04inventoArgentino/ejercicioI04inventoArgentino/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Biblioteca;
+
+namespace ejercicioI04inventoArgentino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Boligrafo biromeAzul = new Boligrafo(ConsoleColor.Blue, 100);
+            Boligrafo biromeRoja = new Boligrafo(ConsoleColor.Red, 50);
+
+            Console.ForegroundColor = biromeAzul.GetColor();
+            Console.WriteLine($"Birome azul nivel de carga: {biromeAzul.GetTinta()}");
+            Console.WriteLine(biromeAzul.Pintar(-10));
+            Console.WriteLine($"Birome azul nivel de carga: {biromeAzul.GetTinta()}");
+            biromeAzul.Recargar();
+            Console.WriteLine($"Birome azul recargada: {biromeAzul.GetTinta()}");
+
+            Console.ForegroundColor = biromeRoja.GetColor();
+            Console.WriteLine($"Birome roja nivel de carga: {biromeRoja.GetTinta()}");
+            Console.WriteLine(biromeRoja.Pintar(-45));
+            Console.WriteLine($"Birome roja nivel de carga: {biromeRoja.GetTinta()}");
+            Console.WriteLine(biromeRoja.Pintar(-1));
+            biromeRoja.Recargar();
+            Console.WriteLine($"Birome roja recargada: {biromeRoja.GetTinta()}");
+            
+
+            
+
+
+        }
+    }
+}
