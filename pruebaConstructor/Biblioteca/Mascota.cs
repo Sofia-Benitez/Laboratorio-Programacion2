1ab9f4df36ac3ddd9146048ef343ebfd605e02ad
Sofia <sofiaaldanabenitez@gmail.com>
2021-08-30 21:17:31 -0300

    clase 3

---
+++ pruebaConstructor/Biblioteca/Mascota.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Veterinaria
+{
+    public class Mascota
+    {
+        private string nombre;
+        private DateTime fechaNacimiento;
+        private string especie;
+
+        public Mascota(string nombre, DateTime fechaNacimiento, string especie)
+        {
+            this.nombre = nombre;
+            this.fechaNacimiento = fechaNacimiento;
+            this.especie = especie;
+        }
+
+        public string GetNombre()
+        {
+            return nombre;
+        }
+
+        public string SetNombre(string nuevoNombre)
+        {
+            return nombre = nuevoNombre;
+        }
+
+        public string GetFechaNacimiento()
+        {
+            return fechaNacimiento.ToShortDateString();
+        }
+
+        public string GetEspecie()
+        {
+            return especie;
+        }
+    }
+}
---
+++ pruebaConstructor/vista/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Veterinaria;
+
+namespace vista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mascota[] mascotas = new Mascota[3];
+
+            Mascota felix = new Mascota("Felix", new DateTime(1990, 01, 01), "gato");
+
+            mascotas[0] = felix;
+            mascotas[1] = new Mascota("Salem", new DateTime(1998, 09, 09), "gato");
+
+            Console.WriteLine($"´{felix.GetNombre()} nacio el {felix.GetFechaNacimiento()} y es un {felix.GetEspecie()}");
+        }
+    }
+}
