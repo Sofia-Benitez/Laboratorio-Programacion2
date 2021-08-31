09c37b78179920250e435becaecb18522cb8f7c4
Sofia <sofiaaldanabenitez@gmail.com>
2021-08-31 16:03:48 -0300

    clase 3

---
+++ ejercicioI02primaveras/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Biblioteca;
+
+namespace ejercicioI02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p1 = new Persona("Ricardo", new DateTime(1988, 08, 28), "38532435");
+            Persona p2 = new Persona("Pollo", new DateTime(1989, 06, 09), "3155435");
+            Persona p3 = new Persona("Chiqui", new DateTime(2009, 07, 01), "45532435");
+
+            p1.SetNombre("Walter");
+            Console.WriteLine(p1.Mostrar());
+            Console.WriteLine(p1.EsMayorDeEdad());
+
+            Console.WriteLine(p2.Mostrar());
+            Console.WriteLine(p2.EsMayorDeEdad());
+
+            Console.WriteLine(p3.Mostrar());
+            Console.WriteLine(p3.EsMayorDeEdad());
+
+
+        }
+    }
+}
