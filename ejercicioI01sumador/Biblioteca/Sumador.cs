42ab1a7440c484e37770019e72da50257a93352c
Sofia <sofiaaldanabenitez@gmail.com>
2021-08-31 21:26:32 -0300

    clase 4

---
+++ ejercicioI01sumador/Biblioteca/Sumador.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    public class Sumador
+    {
+        private int cantidadSumas;
+
+        public Sumador(int cantidad)
+        {
+            cantidadSumas = cantidad;
+        }
+
+        public Sumador():this(0)
+        {
+
+        }
+
+        public int GetCantidadSumas()
+        {
+            return cantidadSumas;
+        }
+
+        public  long Sumar(long valor1, long valor2)
+        {
+            this.cantidadSumas++;
+            return valor1 + valor2;
+        }
+
+        public string Sumar(string valor1, string valor2)
+        {
+            this.cantidadSumas++;
+            return valor1 + valor2;
+        }
+
+        public static explicit operator int(Sumador sumador1)
+        {
+            return sumador1.cantidadSumas;
+        }
+
+        public static long operator +(Sumador sumador1, Sumador sumador2)
+        {
+            return sumador1.cantidadSumas + sumador2.cantidadSumas;
+        }
+
+        public static bool operator | (Sumador sumador1, Sumador sumador2)
+        {
+            if(sumador1.cantidadSumas == sumador2.cantidadSumas)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ ejercicioI01sumador/ejercicioI01sumador/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Biblioteca;
+
+namespace ejercicioI01sumador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sumador sumador1 = new Sumador(6);
+            Sumador sumador2 = new Sumador(6);
+
+            Console.WriteLine(sumador1 | sumador2);
+
+            Console.WriteLine(sumador1.Sumar(4, 5) );
+            Console.WriteLine(sumador2.Sumar("Hola", "mundo"));
+
+            Console.WriteLine(sumador1.GetCantidadSumas());
+            Console.WriteLine(sumador2.GetCantidadSumas());
+
+            Console.WriteLine(sumador1+sumador2);
+
+            Console.WriteLine(sumador1|sumador2);
+        }
+    }
+}
