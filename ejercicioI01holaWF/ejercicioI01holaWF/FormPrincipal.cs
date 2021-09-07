bae072ce4cfbd037282cb9f4e920d3b19cda7419
Sofia <sofiaaldanabenitez@gmail.com>
2021-09-07 13:58:05 -0300

    clase 5

---
+++ ejercicioI01holaWF/ejercicioI01holaWF/FormPrincipal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicioI01holaWF
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSaludar_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+            string titulo = "Hola Windows Form!";
+            string mensaje = $"Soy {nombre} {apellido}";
+            frmMensaje frmMensaje = new frmMensaje(titulo, mensaje);
+            frmMensaje.ShowDialog();
+        }
+    }
+}
---
+++ ejercicioI01holaWF/ejercicioI01holaWF/frmMensaje.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicioI01holaWF
+{
+    public partial class frmMensaje : Form
+    {
+        public frmMensaje(string titulo, string mensaje)
+        {
+            InitializeComponent();
+            lblTitulo.Text = titulo;
+            lblMensaje.Text = mensaje;
+        }
+    }
+}
