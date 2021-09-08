f35579c785288e65f977edf5e680620333aa0bdb
Sofia <sofiaaldanabenitez@gmail.com>
2021-09-07 22:01:19 -0300

    clase 6

---
+++ ejercicioI03contarPalabras/ejercicioI03contarPalabras/FrmContadorPalabras.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace ejercicioI03contarPalabras
+{
+    partial class FrmContadorPalabras
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(41, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(280, 250);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCalcular.Location = new System.Drawing.Point(88, 290);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(182, 32);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // FrmContadorPalabras
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 345);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "FrmContadorPalabras";
+            this.Text = "FrmContadorPalabras";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
---
+++ ejercicioI03contarPalabras/ejercicioI03contarPalabras/FrmContadorPalabras.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicioI03contarPalabras
+{
+    public partial class FrmContadorPalabras : Form
+    {
+        public FrmContadorPalabras()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, int> diccionarioPalabras = new Dictionary<string, int>();
+
+            string texto = richTextBox1.Text;
+            string[] palabras = texto.Split(' ');
+            StringBuilder sb = new StringBuilder();
+            
+            
+            foreach(string palabra in palabras)
+            {
+                //si la palabra ya esta solo incremento el valor
+                if (diccionarioPalabras.ContainsKey(palabra))
+                {
+                    diccionarioPalabras[palabra]++;
+                    
+                }
+                else
+                {
+                    //si no esta la agrego al diccionario
+                    diccionarioPalabras.Add(palabra, 1);
+                    
+                }
+
+                
+            }
+
+            //hago una lista con el diccionario
+            List<KeyValuePair<string, int>> podio = diccionarioPalabras.ToList();
+
+            MessageBox.Show( sb.ToString());
+        }
+    }
+}
