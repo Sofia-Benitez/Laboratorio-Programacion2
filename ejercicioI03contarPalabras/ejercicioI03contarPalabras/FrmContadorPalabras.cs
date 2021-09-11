58665258ebdbe04a80175974c46e0c68d6ad7097
Sofia <sofiaaldanabenitez@gmail.com>
2021-09-11 12:44:41 -0300

    clase 6

--- ejercicioI03contarPalabras/ejercicioI03contarPalabras/FrmContadorPalabras.cs
+++ ejercicioI03contarPalabras/ejercicioI03contarPalabras/FrmContadorPalabras.cs
@@ -19,35 +19,79 @@ namespace ejercicioI03contarPalabras
 
         private void btnCalcular_Click(object sender, EventArgs e)
         {
+
+            Dictionary<string, int> diccionarioPalabras = ObtenerContadorPalabras();
+
+           List<KeyValuePair<string,int>> podio = ObtenerPodio(diccionarioPalabras);
+            MostrarPodio(podio);
+
+        }
+
+        //criterio de ordenamiento para ordenar el podio en ObtenerPodio()
+        private int CompararRepeticiones(KeyValuePair<string, int> primerElemento, KeyValuePair<string,int> segundoElemento)
+        {
+            return segundoElemento.Value - primerElemento.Value;
+        }
+        
+        private List<KeyValuePair<string, int>> ObtenerPodio(Dictionary<string, int> diccionarioPalabras)
+        {
+            //hago una lista con el diccionario
+            List<KeyValuePair<string, int>> podio = diccionarioPalabras.ToList();
+            //ordena con el metodo sort (usa un criterio por defecto o uno que le pasemos como referencia)
+            podio.Sort(CompararRepeticiones);
+
+            return podio;
+            
+        }
+        private void MostrarPodio(List<KeyValuePair<string, int>> podio)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if(podio.Count == 0)
+            {
+                sb.AppendLine("No se ingresaron palabras.");
+            }
+            else
+            {
+                for (int i = 0; i < podio.Count && i<3; i++)
+                {
+                    KeyValuePair<string, int> par = podio[i];
+                    sb.AppendLine($"Palabra: {par.Key} Cantidad: {par.Value}");
+                }
+                //foreach (KeyValuePair<string, int> par in podio)
+                //{
+                //    sb.AppendLine($"Palabra: {par.Key}. Cantidad: {par.Value}");
+                //}
+            }
+
+            
+
+            MessageBox.Show(sb.ToString(), "Podio");
+        }
+        private Dictionary<string, int> ObtenerContadorPalabras()
+        {
             Dictionary<string, int> diccionarioPalabras = new Dictionary<string, int>();
 
             string texto = richTextBox1.Text;
-            string[] palabras = texto.Split(' ');
+            string[] palabras = texto.Split(' ', StringSplitOptions.RemoveEmptyEntries);
             StringBuilder sb = new StringBuilder();
-            
-            
-            foreach(string palabra in palabras)
+
+
+            foreach (string palabra in palabras)
             {
                 //si la palabra ya esta solo incremento el valor
                 if (diccionarioPalabras.ContainsKey(palabra))
                 {
                     diccionarioPalabras[palabra]++;
-                    
                 }
                 else
                 {
                     //si no esta la agrego al diccionario
                     diccionarioPalabras.Add(palabra, 1);
-                    
                 }
-
-                
             }
 
-            //hago una lista con el diccionario
-            List<KeyValuePair<string, int>> podio = diccionarioPalabras.ToList();
-
-            MessageBox.Show( sb.ToString());
+            return diccionarioPalabras;
         }
     }
 }
