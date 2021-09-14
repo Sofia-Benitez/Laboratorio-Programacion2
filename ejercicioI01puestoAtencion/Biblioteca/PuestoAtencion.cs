9bada29a1538adfd6add06f0956ef4c7c3bdc4bf
Sofia <sofiaaldanabenitez@gmail.com>
2021-09-13 22:20:17 -0300

    clase 7

---
+++ ejercicioI01puestoAtencion/Biblioteca/PuestoAtencion.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class PuestoAtencion
+    {
+        
+        private Puesto puesto;
+        private static int numeroActual;
+
+        public enum Puesto
+        {
+            Caja1,
+            Caja2
+        }
+        static PuestoAtencion()
+        {
+            PuestoAtencion.numeroActual = 0;
+        }
+
+        public PuestoAtencion(Puesto puesto)
+        {
+            this.puesto = puesto;
+        }
+
+        public static int NumeroActual
+        {
+            get
+            {
+                PuestoAtencion.numeroActual++;
+                return numeroActual;
+            }
+
+        }
+
+        public bool Atender(Cliente cli)
+        {
+            if(cli is not null)
+            {
+                Thread.Sleep(1000);
+                return true;
+            }
+            return false;
+            
+        }
+
+        
+    }
+
+    
+    
+}
---
+++ ejercicioI01puestoAtencion/ejercicioI01puestoAtencion/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ejercicioI01puestoAtencion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+       
+    }
+}
