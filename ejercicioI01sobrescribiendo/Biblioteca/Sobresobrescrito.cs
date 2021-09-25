45d92872049defe35e7b4c4141cb689fc755e029
Sofia <sofiaaldanabenitez@gmail.com>
2021-09-25 12:09:08 -0300

    clase 9

---
+++ ejercicioI01sobrescribiendo/Biblioteca/Sobresobrescrito.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Sobresobrescrito : Sobrescrito
+    {
+        public Sobresobrescrito() : base()
+        {
+
+        }
+
+        public override string MiPropiedad
+        {
+            get
+            {
+                return miAtributo;
+            }
+        }
+
+        public override string MiMetodo()
+        {
+           return MiPropiedad;
+        }
+    }
+}
---
+++ ejercicioI01sobrescribiendo/ejercicioI01sobrescribiendo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Biblioteca;
+
+namespace ejercicioI01sobrescribiendo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Sobre-Sobrescrito";
+            Sobrescrito sobrecarga = new Sobresobrescrito();
+
+            Console.WriteLine(sobrecarga.ToString());
+
+            string objeto = "¡Este es mi método ToString sobrescrito!";
+
+            Console.WriteLine("----------------------------------------------");
+            Console.Write("Comparación Sobrecargas con String: ");
+            Console.WriteLine(sobrecarga.Equals(objeto));
+
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine(sobrecarga.GetHashCode());
+
+            Console.ReadKey();
+        }
+    }
+}
